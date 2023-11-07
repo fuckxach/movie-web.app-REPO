f177938d313adec64054857d180d53beaabad883
fuckxach <150105783+fuckxach@users.noreply.github.com>
2023-11-07 07:28:00 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "hello-world-muddy-boat-fced.slomomay51.workers.dev",
+  VITE_TMDB_READ_API_KEY: "987702ac1cee74f137afc5a52ddb9fb1"
+};
